e334fbb89e2624308394f6469f9dd428a54c6b95
mankeyrabbit <mankeyrabbit@b63810fd-744e-0410-bb97-ef0cbae2f942>
2008-06-18 08:29:19 +0000

    Tutorial 4

--- src/main.c
+++ src/main.c
@@ -8,6 +8,11 @@
 int main(multiboot_t *mboot_ptr)
 {
   monitor_clear();
-  monitor_write("Hello, world!");
+  monitor_write("Hello, world!\n");
+
+  init_descriptor_tables();
+  asm volatile("int $0x3");
+  asm volatile("int $0x4");
+  
   return 0xdeadbeef;
 }
