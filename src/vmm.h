cf03a8275d09aac0ea22ec6618f639d459697085
mankeyrabbit <mankeyrabbit@b63810fd-744e-0410-bb97-ef0cbae2f942>
2008-11-27 10:53:48 +0000

    Removed redundant page_t declaration

--- src/vmm.h
+++ src/vmm.h
@@ -18,17 +18,6 @@
 #define PAGE_USER      0x4
 #define PAGE_MASK      0xFFFFF000
 
-typedef struct
-{
-  uint32_t present     : 1;  // Page present in memory.
-  uint32_t rw          : 1;  // Read-only if clear, read-write if set.
-  uint32_t user        : 1;  // Supervisor level only if clear.
-  uint32_t accessed    : 1;  // Has the page been accessed since last refresh?
-  uint32_t dirty       : 1;  // Has the page been written to since last refresh?
-  uint32_t unused      : 7;  // Amalgamation of unused and reserved bits.
-  uint32_t frame       : 20; // Frame address (shifted right 12 bits).
-} page_t;
-
 typedef uint32_t page_directory_t;
 
 // Sets up the environment, page directories etc and enables paging.
