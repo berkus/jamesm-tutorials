d11ac06acd7e6f3c7c76dc726650b5364356d449
berkus <berkus@b63810fd-744e-0410-bb97-ef0cbae2f942>
2011-06-04 18:58:08 +0000

    Review up to genesis.

--- src/boot.s
+++ src/boot.s
@@ -7,7 +7,7 @@ MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
 MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
 MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
 ; NOTE: We do not use MBOOT_AOUT_KLUDGE. It means that GRUB does not
-; pass us a symbol table.
+; pass us a symbol table. GRUB will also require our kernel be in elf32 format.
 MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
 MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
@@ -19,7 +19,7 @@ global mboot                    ; Make 'mboot' accessible from C.
 
 mboot:
     dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
-                                ; 4-byte boundary in your kernel file
+                                ; 4-byte boundary in the first 8KB of your kernel file.
     dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
     dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
     
@@ -31,12 +31,12 @@ start:
 
     mov ebp, 0                  ; Initialise the base pointer to zero so we can terminate stack traces
                                 ; here.
-    mov esp, stack
+    mov esp, stack              ; Set up our own stack.
 
     cli                         ; Disable interrupts.
     call main                   ; call our main() function.
     jmp $                       ; Enter an infinite loop, to stop the processor
-.end:                           ; executing whatever rubbish is in the memory
+.end:                           ; from executing whatever rubbish is in the memory
                                 ; after our kernel!
 
 section .bss
